38fc54f1d242b1c222e6e997c571c39dc4c17034
Julita Bielik <julz@o2.pl>
2015-09-20 20:19:06 +0200

    new class

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] documentNames = DocumentCollector.getDocNames();
-		Tokenizer.readDocuments(documentNames);
+		//String[] documentNames = DocumentCollector.getDocNames();
+		//Tokenizer.readDocuments(documentNames);
 		
 		//Search word, document etc
-		HashMap<String, List<Posting>> invertedIndex = new HashMap<String, List<Postring>>();
+		//HashMap<String, List<Posting>> invertedIndex = new HashMap<String, List<Postring>>();
 		
 		
 	}
